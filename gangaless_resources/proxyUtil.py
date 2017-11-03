b92a375c3eeccbd9157a8a716872d0a4f40c9e6f
juacrumar <juacrumar@gmail.com>
2017-11-03 16:13:05 +0000

    recovering proxyutil

---
+++ gangaless_resources/proxyUtil.py
@@ -0,0 +1,26 @@
+#
+# Utilities to deal with ARC and Dirac proxys
+# 
+
+def arcProxy(validity, password = None):
+    from subprocess import call
+    cmdbase = ["arcproxy"]
+    args    = ["-S", "pheno", "-c", "validityPeriod="+validity, "-c","vomsACvalidityPeriod="+validity]
+    if password:
+        pass
+    call(cmdbase + args)
+    return 0
+
+def arcProxyWiz():
+    err = arcProxy("24h")
+    return 0
+
+def diracProxy(password = None):
+    from subprocess import call
+    call(["which", "dirac-proxy-init"])
+    cmdb = ["dirac-proxy-init", "-g", "pheno_user", "-M"]
+    if password:
+        pass
+    call(cmdb)
+    return 0
+
