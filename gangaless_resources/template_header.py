ef042b9cca32084ca7d475351cda40cee655405b
Duncan Walker <duncan_martin_walker@yahoo.co.uk>
2017-11-17 16:24:55 +0000

    Factored out some LFN directory configs into header.py. WARNING - not fully functional, as need to be passed through into ARC/DIRAC.py. Plus I may have missed some somewhere....

--- gangaless_resources/template_header.py
+++ gangaless_resources/template_header.py
@@ -19,6 +19,11 @@ jobName    = "testjob"
 
 # Grid config 
 lfndir   = "/grid/pheno/dwalker"
+# The following options are not fully functional yet 
+# in particular they need to be propagated to DIRAC/ARC.py via ?cmd line args?
+# I may also have missed some hardcodings
+lfn_output_dir = "output"
+lfn_warmup_dir = "warmup"
 
 # Lhapdf config
 lhapdf_grid_loc = "input/" 
