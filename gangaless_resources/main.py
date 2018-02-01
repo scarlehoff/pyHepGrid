d2252b7ae6f8b8bcd2bf4601b048fe73a620f5ca
juacrumar <juacrumar@gmail.com>
2018-02-01 11:35:53 +0000

    -j all now allows to use -A and -D at the same time, so looping through both Dirac and Arc tables is allowed

--- gangaless_resources/main.py
+++ gangaless_resources/main.py
@@ -5,13 +5,16 @@ from argument_parser import arguments as args
 rcard = args.runcard
 rmode = args.mode
 
+from header import arctable, diractable, dbname, dbfields
+from dbapi  import database
 
 ##### Checks go here
 if len(rmode) < 3:
     raise Exception("Mode ", rmode, " not valid")
 if rmode[:3] == "run" or rmode[:3] == "man":
     if args.runDirac and args.runArc:
-        raise Exception("Please, choose only Dirac (-D) or Arc (-A) or Arc Production Mode (-B)")
+        if not args.idjob == "all":
+            raise Exception("Please, choose only Dirac (-D) or Arc (-A) or Arc Production Mode (-B) (unless using -j all)")
     if not args.runDirac and not args.runArc and not args.runArcProduction:
         raise Exception("Please , choose either Dirac (-D) or Arc (-A) or Arc Production Mode (-B)")
 
@@ -26,8 +29,6 @@ if not args.noProxy:
 ########################################
 
 #### Step2, generate database and tables
-from header import arctable, diractable, dbname, dbfields
-from dbapi  import database
 db = database(dbname, tables = [arctable, diractable], fields = dbfields)
 ########################################
 
@@ -59,86 +60,26 @@ elif rmode[:3] == "run":
     else:
         raise Exception("Choose what do you want to run -(A/B/D)")
     runWrapper(rcard, args.test)
+
+
 #### Management: 
 elif rmode[:3] == "man":
-    if args.runArc or args.runArcProduction:
-        from backendManagement import Arc as backend_class
-    if args.runDirac: 
-        from backendManagement import Dirac as backend_class
-    backend = backend_class(act_only_on_done = args.done)
-    if args.yes:
-        backend.dont_ask_dont_tell()
-
-    from header import finalisation_script
-    if args.get_data and finalisation_script:
-        backend.get_data(0, custom_get = finalisation_script)
-        exit(0)
-
-    if args.updateArc:
-        if not args.runArc: 
-            raise Exception("Update ARC can only be used with ARC")
-        backend.update_stdout()
-        exit(0)
-    if args.idjob:
-        id_str = args.idjob
+    import main_routines as mr
+    if (args.runArc or args.runArcProduction) and args.runDirac:
+        print(" > > > Looping thorugh ARC runs: ")
+        from backendManagement import Arc as backend_Arc
+        backend = backend_Arc(act_only_on_done = args.done)
+        mr.management_routine(backend, args)
+        print(" > > > Looping thorugh Dirac runs: ")
+        from backendManagement import Dirac as backend_Dirac
+        backend = backend_Dirac(act_only_on_done = args.done)
+        mr.management_routine(backend, args)
     else:
-        backend.list_runs(args.find)
-        id_str = input("> Select id to act upon: ")
+        if args.runArc or args.runArcProduction:
+            from backendManagement import Arc as backend_class
+        if args.runDirac: 
+            from backendManagement import Dirac as backend_class
+        backend = backend_class(act_only_on_done = args.done)
+        mr.management_routine(backend, args)
 
-    id_list_raw = str(id_str).split(",")
-    id_list = []
-    if len(id_list_raw) == 1 and id_list_raw[0].lower() == "all":
-        id_list_raw = backend.get_active_dbids()
-    for id_selected in id_list_raw:
-        if "-" in id_selected:
-            id_limits = id_selected.split("-")
-            for id_int in range(int(id_limits[0]), int(id_limits[1]) + 1):
-                id_list.append(str(id_int))
-        else:
-            id_list.append(id_selected)
-
-    for db_id in id_list:
-        jobid = backend.get_id(db_id) # a list
-        # Options that keep the database entry after they are done
-        if args.stats:
-            backend.stats_job(db_id)
-        elif args.statsCheat:
-            backend.stats_job_cheat(db_id)
-        elif args.info or args.infoVerbose:
-            print("Retrieving information . . . ")
-            backend.status_job(jobid, args.infoVerbose)
-        elif args.renewArc:
-            print("Renewing proxy for the job . . . ")
-            backend.renew_proxy(jobid)
-        elif args.printme:
-            print("Printing information . . . ")
-            backend.cat_job(jobid, print_stderr = args.error)
-        elif args.printmelog:
-            print("Printing information . . . ")
-            backend.cat_log_job(jobid)
-        elif args.getmewarmup:
-            print("Retrieving warmup")
-            backend.bring_current_warmup(db_id)
-
-        # Options that deactivate the database entry once they're done
-        elif args.get_data:
-            print("Retrieving job data")
-            backend.get_data(db_id)
-            if not args.done: # if --done is used we assume there are jobs which are _not_ done
-                backend.disable_db_entry(db_id)
-        elif args.kill_job:
-            print("Killing the job")
-            backend.kill_job(jobid)
-            backend.disable_db_entry(db_id)
-        elif args.clean:
-            print("Cleaning job . . . ")
-            backend.clean_job(jobid)
-            backend.disable_db_entry(db_id)
-        # Enable back any database entry
-        elif args.enableme:
-            backend.enable_db_entry(db_id)
-        elif args.disableme:
-            backend.disable_db_entry(db_id)
-        else:
-            print(jobid)
-
+    
