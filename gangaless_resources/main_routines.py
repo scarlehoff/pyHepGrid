06381d23a99f1429b62c2862b1e6541902cdfcb8
Duncan Walker <duncan_martin_walker@yahoo.co.uk>
2018-02-14 16:57:52 +0000

    Got bored at Higgs-Maxwell and prettified printing in main_routines

--- gangaless_resources/main_routines.py
+++ gangaless_resources/main_routines.py
@@ -20,7 +20,7 @@ def management_routine(backend, args):
         id_str = args.idjob
     else:
         backend.list_runs(args.find)
-        id_str = input("> Select id to act upon: ")
+        id_str = input("> Select id to act on: ")
 
     id_list_raw = str(id_str).split(",")
     id_list = []
@@ -34,49 +34,61 @@ def management_routine(backend, args):
         else:
             id_list.append(id_selected)
 
-    for db_id in id_list:
+    no_ids = len(id_list)
+
+    for idx,db_id in enumerate(id_list):
+        # Setup for printing/function args
+        jdx= idx+1
+        jobinfo = backend.dbase.list_data(backend.table, ["runcard", "jobtype"], db_id)[0]
+        jobname = "{0} ({1})".format(jobinfo["runcard"],jobinfo["jobtype"])
         jobid = backend.get_id(db_id) # a list
+        printstr = "{0} for job"+" {0}: {3:20} [{1}/{2}]".format(db_id,jdx,no_ids,jobname)
+
+
+        # Could we make this more generic? i.e pass function with opt args using a dictionary
+        # rather than just making copies for every possibility
         # Options that keep the database entry after they are done
         if args.stats:
             backend.stats_job(db_id)
         elif args.statsCheat:
             backend.stats_job_cheat(db_id)
         elif args.info or args.infoVerbose:
-            print("Retrieving information . . . ")
+            print(printstr.format("Retrieving information"))
             backend.status_job(jobid, args.infoVerbose)
         elif args.renewArc:
-            print("Renewing proxy for the job . . . ")
+            print(printstr.format("Renewing proxy"))
             backend.renew_proxy(jobid)
         elif args.printme:
-            print("Printing information . . . ")
+            print(printstr.format("Printing information"))
             backend.cat_job(jobid, print_stderr = args.error)
         elif args.printmelog:
-            print("Printing information . . . ")
+            print(printstr.format("Printing information from logfile"))
             backend.cat_log_job(jobid)
         elif args.getmewarmup:
-            print("Retrieving warmup")
+            print(printstr.format("Retrieving warmup"))
             backend.bring_current_warmup(db_id)
 
         # Options that deactivate the database entry once they're done
         elif args.get_data:
-            print("Retrieving job data")
+            print(printstr.format("Retrieving data"))
             backend.get_data(db_id)
             if not args.done: # if --done is used we assume there are jobs which are _not_ done
                 backend.disable_db_entry(db_id)
         elif args.kill_job:
-            print("Killing the job")
+            print(printstr.format("Killing"))
             backend.kill_job(jobid)
             backend.disable_db_entry(db_id)
         elif args.clean:
-            print("Cleaning job . . . ")
+            print(printstr.format("Cleaning"))
             backend.clean_job(jobid)
             backend.disable_db_entry(db_id)
+
         # Enable back any database entry
         elif args.enableme:
             backend.enable_db_entry(db_id)
         elif args.disableme:
             backend.disable_db_entry(db_id)
         else:
-            print(jobid)
+            print(" ".join(i for i in jobid))
 
 
