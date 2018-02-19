e0578a072b05d2a932d429e5333f2dffd23b38f1
Duncan Walker <duncan_martin_walker@yahoo.co.uk>
2018-02-19 12:21:09 +0000

    added template finialise_runcard to repo

---
+++ runcards/template_finalise_runcard.py
@@ -0,0 +1,26 @@
+import sys, os, importlib
+# Auto select all runcards
+runcard_files = os.listdir(os.path.dirname(os.path.abspath(__file__)))
+runcards = [rc.replace(".py","") for rc in runcard_files if rc.endswith(".py") 
+
+# Or you can hard code them here...
+#runcards = ["triple_differential"]
+
+            and "template" not in rc and "finalise" not in rc]
+# Disable
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+# Restore
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
+# RUNCARDS USED ONLY FOR PULLING DATA WITH FINALISE.PY 
+print("Pulling data for runcard(s): {0}".format("".join(i for i in runcards)))
+blockPrint()
+modules = [importlib.import_module("runcards.{0}".format(rc)) for rc in runcards]
+enablePrint()
+
+dictCard = {}
+for x in modules:
+    dictCard.update(x.dictCard)
