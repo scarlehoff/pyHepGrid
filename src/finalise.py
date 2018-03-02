72877e49c10c7b779cc534b563ce75475151aa79
Duncan Walker <duncan_martin_walker@yahoo.co.uk>
2018-03-02 12:37:51 +0000

    consolidated the number of mkdirs/chdirs/rmtrees in finalse.py

--- src/finalise.py
+++ src/finalise.py
@@ -39,14 +39,12 @@ def get_output_dir_name(runcard):
 def createdirs(currentdir, runcard):
     targetdir = os.path.join(currentdir, get_output_dir_name(runcard))
     mkdir(targetdir)
-    newdir = os.path.join(targetdir, 'log')
-    mkdir(newdir)
-
     logdir = os.path.join(targetdir, 'log')
-    os.chdir(logdir)
-    logcheck = glob.glob('*.log')
-
-    return logcheck, targetdir
+    mkdir(logdir)
+    tmpdir = os.path.join(targetdir, '.tmp')
+    mkdir(tmpdir)
+    logcheck = glob.glob('{0}/*.log'.format(logdir))
+    return logcheck, targetdir, tmpdir
 
 
 def seed_present(logcheck, seedstr):
@@ -56,14 +54,12 @@ def seed_present(logcheck, seedstr):
 def pullrun(name, seed, run, output, logcheck, tmpdir):
     seedstr = ".s{0}.log".format(seed)
     if name in output and not seed_present(logcheck, seedstr):
-        os.mkdir(tmpdir)
         os.chdir(tmpdir)
         status = 0
         print("Pulling {0}, seed {1}".format(run, seed))
         command = 'lcg-cp lfn:output/{0} {0} 2>/dev/null'.format(name)
         os.system(command)
         # Use python tar extraction here
-        #out = os.system('tar -xf ' + name + ' -C .')
         corrupted = True
         with tarfile.open(name, 'r|gz') as tfile:
             for t in tfile:
@@ -78,13 +74,10 @@ def pullrun(name, seed, run, output, logcheck, tmpdir):
             # Hits if seed not found in any of the output files
             print("Deleting {0}, seed {1}. Corrupted output".format(run, seed))
             os.system('lcg-del -a lfn:output/{0}'.format(name))
-        shutil.rmtree(tmpdir)
         
 
 def pull_seed_data(rc_tar, runcard, output, logcheck, targetdir):
-    pid = mp.current_process().pid
-    # Separate tmpdir for each process
-    tmpdir = os.path.join(targetdir, '.process_{0}_tmp'.format(pid))
+    tmpdir = os.path.join(targetdir, ".tmp")
     seed = rc_tar.split(".")[-3].split("-")[-1]
     pullrun(rc_tar, seed, runcard, output, logcheck, tmpdir)
 
@@ -93,10 +86,10 @@ def do_finalise():
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
     os.chdir(dname)
-    cmd = ['lfc-ls', 'output']
+    cmd = ['lfc-ls', config.lfn_output_dir]
     output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
     currentdir = os.getcwd()
-    output = [x for x in str(output).split("\\n")]
+    output = set([x for x in str(output).split("\\n")])
 
     pool = mp.Pool(processes=no_processes)
     tot_rc_no = len(rc.dictCard)
@@ -105,11 +98,11 @@ def do_finalise():
         dirtag = runcard + "-" + rc.dictCard[runcard]
         runcard_name_no_seed = "output{0}-".format(dirtag)
         runcard_output = [i for i in output if runcard_name_no_seed in i]
-        logcheck, targetdir = createdirs(currentdir, dirtag)
+        logcheck, targetdir, tmpdir = createdirs(currentdir, dirtag)
         pool.starmap(pull_seed_data, zip(runcard_output, it.repeat(runcard), 
                                          it.repeat(output), it.repeat(logcheck),
                                          it.repeat(targetdir)))
-
+        shutil.rmtree(tmpdir)
 
 if __name__ == "__main__":
     do_finalise()
