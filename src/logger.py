6fb9d01a145273eae3363ae0f4225d7d01742388
Duncan Walker <duncan_martin_walker@yahoo.co.uk>
2018-03-28 13:05:55 +0100

    Added minor cmd line arg warning changes

--- src/logger.py
+++ src/logger.py
@@ -1,14 +1,29 @@
 import logging
 import sys
 
-def setup_logger():
+logging.VALUES = 15
+logging.addLevelName(logging.VALUES, "VALUES")
+
+def values(self, message, *args, **kws):
+    # Yes, logger takes its '*args' as 'args'.
+    if self.isEnabledFor(logging.VALUES):
+        self._log(logging.VALUES, message, args, **kws) 
+
+logging.Logger.values = values
+
+def setup_logger(debuglevel):
+
+    # Add custom level for value initialisation
+
     logger = logging.getLogger(__name__)
     hdlr = logging.StreamHandler(sys.stdout)
     formatter = MyFormatter()
- 
     hdlr.setFormatter(formatter)
     logger.addHandler(hdlr)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(debuglevel)
+
+
+
     return logger
 
 class MyFormatter(logging.Formatter):
@@ -16,9 +31,10 @@ class MyFormatter(logging.Formatter):
     format_strs  ={
             logging.WARNING:  "  \033[93m WARNING:\033[0m {}",
             logging.ERROR:  "  \033[91m ERROR:\033[0m {}",
-            logging.CRITICAL:  "  \033[91m CRITICAL:\033[0m {}",
+            logging.CRITICAL:  "  \033[91m CRITICAL ERROR:\033[0m {}",
             logging.INFO:  "> {}",
             logging.DEBUG:  "  \033[94m DEBUG:\033[0m {}",
+            logging.VALUES:  "\033[92mValue set:\033[0m {}",
             }
 
     def format(self, record):
