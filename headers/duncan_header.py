0ec6ad81585669640586b8a82c4969a50636ea48
Duncan Walker <duncan_martin_walker@yahoo.co.uk>
2018-05-30 12:24:53 +0100

    Update to Dirac submission so that if one sets jobName=None, dCards[r] is used for the jobName instead

--- headers/duncan_header.py
+++ headers/duncan_header.py
@@ -76,7 +76,7 @@ finalise_prefix = "results_"
 # socket parameters
 server_host = "gridui2.dur.scotgrid.ac.uk"
 port = 9000
-wait_time = 3600 # default waiting time for the socket server (time between the first job activates and nnlojet starting to run)
+wait_time = 10800 # default waiting time for the socket server (time between the first job activates and nnlojet starting to run)
 
 #SLURM parameters
 local_run_directory = "/ddn/data/qpsv27/run_directories/"
