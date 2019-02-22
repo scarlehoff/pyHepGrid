a5f2b21e40a7fa240813d8559004268ee60891c9
juacrumar <juacrumar@gmail.com>
2019-02-22 10:32:49 +0100

    first attempt

--- headers/template_header.py
+++ headers/template_header.py
@@ -8,9 +8,10 @@ def get_cmd_output(*args,**kwargs):
     outbyt = sp.Popen(args, stdout=sp.PIPE,**kwargs).communicate()[0]
     return outbyt.decode("utf-8")
 
+# Global Variables
 # Global Variables (default values, can be changed by runcard.py)
-runcardDir = "/mt/home/dwalker/NNLOJET/driver/grid/" # Directory for grid runcard storage
-NNLOJETdir = "/mt/home/dwalker/NNLOJET/"             # Directory for NNLOJET
+runcardDir = "/path/to/runcard/directory/" # Directory for grid runcard storage
+NNLOJETdir = "/path/to/nnlojet/directory"             # Directory for NNLOJET
 NNLOJETexe = "NNLOJET"                               # Exectuable name
 warmupthr  = 8
 producRun  = 100
@@ -19,27 +20,27 @@ jobName    = "testjob"
 debug_level = 0
 stacksize = 50 #MB
 
-# Grid config 
-lfndir         = "/grid/pheno/dwalker"
+# Grid config for LFN
+lfndir         = "/grid/group/user/folder/"
 lfn_input_dir  = "input"
 lfn_output_dir = "output"
 lfn_warmup_dir = "warmup"
 
 use_gfal = False
-gfaldir = "gsiftp://se01.dur.scotgrid.ac.uk/dpm/dur.scotgrid.ac.uk/home/pheno/dwalker/"
+gfaldir = "gsiftp://gfalurl"
 
 # TMUX config
 tmux_location= "tmux"
 
 # Lhapdf config
-lhapdf_grid_loc    = "util/lhapdf.tar.gz"
+lhapdf_grid_loc    = "location/relative/to/lfndir/lhapdf.tar.gz"
 lhapdf_loc         = "lhapdf"
 lhapdf_ignore_dirs = [] # Don't tar up all of LHAPDF if you don't want to
 lhapdf_central_scale_only = True # Only tar up central [0000.dat] PDF sets
 lhapdf             = get_cmd_output("lhapdf-config","--prefix")
  
 # NNLOJET Database Parameters
-dbname     = "NNLOJET_database.dat"     
+dbname     = "/pat/to/sqlite/database/for/storage.dat"     
 provided_warmup_dir = None
 
 # Finalisation and storage options
@@ -60,32 +61,32 @@ production_base_dir = "/ResultsRunGrids"
 short_stats = False
 
 # ARC parameters
-ce_base = "ce2.dur.scotgrid.ac.uk"
-ce_test = "ce-test.dur.scotgrid.ac.uk"
+ce_base = "base_computing_element.ac.uk"
+ce_test = "test_computing_element.ac.uk"
 ce_listfile = "computing_elements.txt"
-arcbase  = "/mt/home/dwalker/.arc/jobs.dat" # arc database
+arcbase  = "/path/to/ARC/base/.arc/jobs.dat" # arc database
 
 # DIRAC parameters
-dirac_name = "duncan.walker"
-DIRAC_BANNED_SITES = ["VAC.UKI-SCOTGRID-GLASGOW.uk"]
+dirac_name = "user_name_for_dirac"
+DIRAC_BANNED_SITES = []
 
 # finalise.py-only parameters
 finalise_runcards = None
 finalise_prefix = None
 
 # socket default parameters
-server_host = "gridui1.dur.scotgrid.ac.uk"
+server_host = "url.of.the.socket.server"
 port = 9999
 wait_time = 3600 # default waiting time for the socket server (time between the first job activates and nnlojet starting to run)
 
 #SLURM parameters
-local_run_directory = "/ddn/data/qpsv27/run_directories/"
-warmup_queue = "openmp7.q"
-test_queue = "test.q"
-production_queue = "par7.q"
+local_run_directory = "/path/to/local/working/dir/"
+warmup_queue = "nameOfWarmupQueue"
+test_queue = "nameOfTestQueue"
+production_queue = "nameOfProductionQueue"
 production_threads = 24
 slurm_exclusive = True
 slurm_exclude = []
 
 # LOCAL
-desktop_list = ["ws1"]
+desktop_list = []
