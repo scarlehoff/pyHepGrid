e1b42f62d16e21b1a9065b0061d2f6d660591520
Marian Heil <48517413+marianheil@users.noreply.github.com>
2019-08-19 18:32:16 +0100

    Init for pure Sherpa

--- src/pyHepGrid/src/runmodes.py
+++ src/pyHepGrid/src/runmodes.py
@@ -2,4 +2,5 @@ import pyHepGrid.src.programs
 
 mode_selector = { "NNLOJET": pyHepGrid.src.programs.NNLOJET,
                   "HEJ": pyHepGrid.src.programs.HEJ,
+                  "Sherpa": pyHepGrid.src.programs.Sherpa,
 }
