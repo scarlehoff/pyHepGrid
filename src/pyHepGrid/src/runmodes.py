c49de344c8990b90c39a22f98406aeb01021cd7d
Marian Heil <48517413+marianheil@users.noreply.github.com>
2019-08-22 11:44:30 +0100

    case insensitive runmode
    Thanks DWalker487

--- src/pyHepGrid/src/runmodes.py
+++ src/pyHepGrid/src/runmodes.py
@@ -2,5 +2,5 @@ import pyHepGrid.src.programs
 
 mode_selector = { "NNLOJET": pyHepGrid.src.programs.NNLOJET,
                   "HEJ": pyHepGrid.src.programs.HEJ,
-                  "Sherpa": pyHepGrid.src.programs.Sherpa,
+                  "SHERPA": pyHepGrid.src.programs.Sherpa,
 }
