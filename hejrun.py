scarlehoff/pyHepGrid

commit cac5c91090c3af4e7d610ed910f6a70597f07246

print program output also to screen (this time for real)

hejrun.py
@@ -168,7 +168,7 @@ lfn    = "lfn:"
 
 
 def run_command(command):
-    command += " 1>> {0} 2>&1".format(LOG_FILE)
+    command += " 2>&1 | tee -a {0} ".format(LOG_FILE)
     print_flush(" > Executed command: {0} ({1})".format(command,
         datetime.datetime.now()))
     return os.system(command)

commit f42be5f3628f5a49e24f8802193a17aa3e5fcddb

Correctly return error code for shell piping
Bug introduced in cac5c91090c3af4e7d610ed910f6a70597f07246

hejrun.py
@@ -168,7 +168,9 @@ lfn    = "lfn:"
 
 
 def run_command(command):
-    command += " 2>&1 | tee -a {0} ".format(LOG_FILE)
+    "catch output in LOG_FILE"
+    # Avoid overwriting of the status code for piping to tee
+    command = 'bash  -o pipefail -c "{0}  2>&1 | tee -a {1}"'.format(command, LOG_FILE)
     print_flush(" > Executed command: {0} ({1})".format(command,
         datetime.datetime.now()))
     return os.system(command)