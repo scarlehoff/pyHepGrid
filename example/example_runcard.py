072758054cde872e93bef92aedd87ce33b490c9e
James Whitehead <whiteheadj@gmail.com>
2020-03-03 19:32:38 +0000

    Example runcard

---
+++ example/example_runcard.py
@@ -0,0 +1,55 @@
+import os
+print("Sourcing runcard")
+dictCard = {
+            "dummy_folder" : "config",
+}
+# for the example, keep everything in 'example' folder.
+exampleDir = os.path.realpath(__file__)
+
+## Run Variables
+
+# run-specific variables (overrides header.py variables)
+runcardDir = os.path.realpath(__file__)
+# number of concurrent jobs to submit
+producRun = 5
+# ARC jobname to appear in squeue output
+jobName = "EXAMPLE_RUN"
+
+# ARC's jobs database: put this wherever you want
+arcbase = os.path.expanduser("~/.arc/jobs.dat")
+# pyHepGrid SQL database
+dbname = F"{exampleDir}/ARCdb_example.dat"
+
+
+# user-specific global variables (could go in user's header)
+copylog = True
+
+# automatically pick next unused seed
+import pyHepGrid.src.dbapi as dbapi
+baseSeed = dbapi.get_next_seed(dbname=dbname) + 1
+# baseSeed = 2173  # (setting seed manually)
+
+## Project-level Variables
+
+# Your custom subclass of ProgramInterface class in program_interface.py
+runmode = "example_program.ProgramClass"
+
+# path to your executable: src_dir/exe
+executable_src_dir = exampleDir
+executable_exe = "examplerun.sh"
+
+grid_input_dir  = "example/input"
+grid_output_dir = "example/output"
+#grid_warmup_dir = "example/warmup" # not used
+
+provided_warmup_dir = runcardDir
+
+
+## User Variables
+
+# your DIRAC username: only needed if you run on DIRAC.
+dirac_name = "your.username"
+
+# custom post-run local download (and processing) script
+# finalisation_script = "path/to/your/script.py"
+
