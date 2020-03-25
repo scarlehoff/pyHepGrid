8644dc4e0f20bb11ad281b5d33a173eade1073df
Marian Heil <48517413+marianheil@users.noreply.github.com>
2020-03-25 15:12:10 +0100

    Added `.py` ending

--- bin/result_stats.py
+++ bin/result_stats.py
@@ -5,7 +5,8 @@ import time
 import argparse as ap
 # Janky af, but allows script to be called from other directories
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-from pyHepGrid.src.header import production_base_dir as rootdir
+from pyHepGrid.src.header import production_base_dir as rootdir  # noqa: E402
+
 
 def do_search(args, path):
     if args.search is not None:
@@ -17,6 +18,7 @@ def do_search(args, path):
                 if searchstr not in path:
                     return False
     return True
+
 
 def do_reject(args, path):
     if args.reject is not None:
@@ -35,6 +37,7 @@ def search_matches_path(args, path):
         return False
     return True
 
+
 def print_output(stats, do_total):
     print(RuncardStats.output_header())
     print(RuncardStats.divider_string())
@@ -44,50 +47,49 @@ def print_output(stats, do_total):
         print(i)
     if do_total:
         print(RuncardStats.divider_string())
-        print(RuncardStats.print_format("Total Files",no_files,""))
+        print(RuncardStats.print_format("Total Files", no_files, ""))
 
 
 def get_stats(args):
     stats = []
     for thing in os.listdir(rootdir):
-        fullpath = os.path.join(rootdir,thing)
+        fullpath = os.path.join(rootdir, thing)
         if os.path.isdir(fullpath):
             if search_matches_path(args, fullpath):
                 try:
-                    retval = RuncardStats(fullpath,thing,args)
+                    retval = RuncardStats(fullpath, thing, args)
                     stats.append(retval)
                 except OSError as e:
                     pass
     return stats
 
 
-
-
 class RuncardStats():
 
     print_string = "{0:<{runcard}} | {1:<{no_files}} | {2:<{modified_time}}"
     tag_flag = False
-    column_names = {"runcard":"Runcard Name","tag":"Tag", # Identifies attr names with column titles
-               "no_files":"No. files", "modified_time":"Last Modified" } # Currently unused
-    pads = {"runcard":5,"tag":5, "no_files":9, "modified_time":20 }
+    column_names = {"runcard": "Runcard Name", "tag": "Tag",  # Identifies attr names with column titles
+                    "no_files": "No. files", "modified_time": "Last Modified"}  # Currently unused
+    pads = {"runcard": 5, "tag": 5, "no_files": 9, "modified_time": 20}
 
     def print_format(*args):
         return RuncardStats.print_string.format(*args, **RuncardStats.pads)
 
     def divider_string():
-        return "="*len(RuncardStats.print_format(0,0,0,0))
+        return "="*len(RuncardStats.print_format(0, 0, 0, 0))
 
     def adjust_column_widths(self):
         for key in RuncardStats.pads:
-            RuncardStats.pads[key] = max(RuncardStats.pads[key],len(str(getattr(self,key))))
+            RuncardStats.pads[key] = max(
+                RuncardStats.pads[key], len(str(getattr(self, key))))
 
-
-    def __init__(self,fullpath, directory, parseargs):
+    def __init__(self, fullpath, directory, parseargs):
         self.directory = directory
-        self.logpath = os.path.join(fullpath,"log/")
+        self.logpath = os.path.join(fullpath, "log/")
         self.logfiles = os.listdir(self.logpath)
         self.no_files = len(self.logfiles)
-        self.runcard = self.directory.replace("results_","").split(".run-")[0]+".run"
+        self.runcard = self.directory.replace(
+            "results_", "").split(".run-")[0]+".run"
         try:
             self.tag = self.directory.split(".run-")[1]
         except IndexError as e:
@@ -100,36 +102,44 @@ class RuncardStats():
             RuncardStats.tag_flag = True
         self.adjust_column_widths()
 
-
     def output_header():
         if not RuncardStats.tag_flag:
-            retstr = RuncardStats.print_format("Runcard Name","No. Files", "Last Modified")
+            retstr = RuncardStats.print_format(
+                "Runcard Name", "No. Files", "Last Modified")
         else:
-            retstr = RuncardStats.print_format("Runcard Name","Tag","No. Files", "Last Modified")
+            retstr = RuncardStats.print_format(
+                "Runcard Name", "Tag", "No. Files", "Last Modified")
         return retstr
 
     def __repr__(self):
         if not self.args.tag:
             retstr = RuncardStats.print_format(
-                self.runcard,self.no_files, self.modified_time )
+                self.runcard, self.no_files, self.modified_time)
         else:
             retstr = RuncardStats.print_format(
-                self.runcard,self.tag,self.no_files, self.modified_time )
+                self.runcard, self.tag, self.no_files, self.modified_time)
         return retstr
+
 
 def parser_setup():
     parser = ap.ArgumentParser(description='Analyse results output directory.')
-    parser.add_argument('--rev', help='reverse sort.', action='store_false',default = True)
-    parser.add_argument('--runcard','-rc', help='sort by runcard name.',action='store_true')
-    parser.add_argument('--tag','-t', help='display runcard tags.',action='store_true')
-    parser.add_argument('--nofiles','-n', help='sort by no files.',action='store_true')
-    parser.add_argument('--modtime','-m', help='sort by modification time.',action='store_true')
-    parser.add_argument('--case_insensitive','-i', help='case insensitive search/reject.',action='store_true')
-    parser.add_argument('--search','-f','-s',
-                        help='search for specific string(s) in runcard dir.', nargs = '+')
-    parser.add_argument('--reject','-r',
-                        help='reject specific string(s) in runcard dir.', nargs = '+')
-    parser.add_argument('--total','-tot',
+    parser.add_argument('--rev', help='reverse sort.',
+                        action='store_false', default=True)
+    parser.add_argument('--runcard', '-rc',
+                        help='sort by runcard name.', action='store_true')
+    parser.add_argument(
+        '--tag', '-t', help='display runcard tags.', action='store_true')
+    parser.add_argument('--nofiles', '-n',
+                        help='sort by no files.', action='store_true')
+    parser.add_argument('--modtime', '-m',
+                        help='sort by modification time.', action='store_true')
+    parser.add_argument('--case_insensitive', '-i',
+                        help='case insensitive search/reject.', action='store_true')
+    parser.add_argument('--search', '-f', '-s',
+                        help='search for specific string(s) in runcard dir.', nargs='+')
+    parser.add_argument('--reject', '-r',
+                        help='reject specific string(s) in runcard dir.', nargs='+')
+    parser.add_argument('--total', '-tot',
                         help='include total # of results', action="store_true")
     args = parser.parse_args()
     return args
@@ -139,12 +149,12 @@ if __name__ == "__main__":
     args = parser_setup()
     stats = get_stats(args)
     if args.runcard:
-        stats.sort(reverse=args.rev,key=lambda x: x.directory)
+        stats.sort(reverse=args.rev, key=lambda x: x.directory)
     if args.modtime:
-        stats.sort(reverse=args.rev,key=lambda x: x.modified_time)
+        stats.sort(reverse=args.rev, key=lambda x: x.modified_time)
     if args.nofiles:
-        stats.sort(reverse=args.rev,key=lambda x: x.nofiles)
-    if len(stats)>0:
-        print_output(stats,args.total)
+        stats.sort(reverse=args.rev, key=lambda x: x.nofiles)
+    if len(stats) > 0:
+        print_output(stats, args.total)
     else:
         print("No results found.")
